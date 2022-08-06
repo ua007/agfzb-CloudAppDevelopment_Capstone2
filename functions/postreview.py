753b5a2a52ca021b9f8b0ef9ee4a2f5e110bd39c
jcruzem <109934669+jcruzem@users.noreply.github.com>
2022-08-06 18:06:54 +0800

    Update postreview.py

--- functions/postreview.py
+++ functions/postreview.py
@@ -1,22 +1,4 @@
 # IBM Action, method=GET, Python 3.9
-# API https://4ea3b251.us-south.apigw.appdomain.cloud/postreview
-# Params
-# {
-# "review": 
-#     {
-#         "id": 1114,
-#         "name": "Upkar Lidder",
-#         "dealership": 15,
-#         "review": "Great service!",
-#         "purchase": false,
-#         "another": "field",
-#         "purchase_date": "02/16/2021",
-#         "car_make": "Audi",
-#         "car_model": "Car",
-#         "car_year": 2021
-#     }
-# }
-# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 import sys
 from ibmcloudant.cloudant_v1 import CloudantV1
@@ -27,10 +9,10 @@ from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 def main(dict):
     # my IAM_API_KEY
-    authenticator = IAMAuthenticator("Um1oyQP-JDmtWBQc90jbhv1EEz2-VgjSqkK-RIcpOkZe")
+    authenticator = IAMAuthenticator("ng9v3anfOCobH63DPcohiQERqqu4Bdcm-oXVH40ntjU5")
     service = CloudantV1(authenticator=authenticator)
     # my COUCH_URL
-    service.set_service_url("https://ab9a3133-c458-4795-8041-55b2ad164a33-bluemix.cloudantnosqldb.appdomain.cloud")
+    service.set_service_url("https://20c446e9-b7d6-47c4-9aba-260d13815831-bluemix.cloudantnosqldb.appdomain.cloud")
     response = service.post_document(db='reviews', document=dict["review"]).get_result()
     try:
     # result_by_filter=my_database.get_query_result(selector,raw_result=True)
